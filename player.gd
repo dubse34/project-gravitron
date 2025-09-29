b446791c91a3f2c3b62a2f9fa896c31f740493e6
Samuel <craftysamuel@gmail.com>
2025-09-29 23:14:24 +0930

    feat: player moment zero g with air resistance

--- player.gd
+++ player.gd
@@ -1,24 +1,31 @@
 extends Area2D
 
-@export var speed = 400 # How fast the player will move (pixels/sec).
+@export var acceleration = 50 # How fast the player will accelerate (pixels/sec).
+
+@export var drag_coefficient = 5 # the deceleration coefficient
+
 var screen_size # Size of the game window.
+
+var velocity = Vector2.ZERO # The player's movement vector.
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	
 func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
+		velocity.x += acceleration
 	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
+		velocity.x -= acceleration
 	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
+		velocity.y += acceleration
 	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
+		velocity.y -= acceleration
+
+	velocity.x += 0.5 * drag_coefficient / 100000 * velocity.x * abs(velocity.x) * -1 #creates wind resistance in opposite direction to motion 1/2*p*v^2*cd
+	velocity.y += 0.5 * drag_coefficient / 100000 * velocity.y * abs(velocity.y) * -1 #creates wind resistance in opposite direction to motion
 
 	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
+		#velocity = velocity.normalized() * speed
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
