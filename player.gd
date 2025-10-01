33fbae7f47cdb8188dd39549288025a4579170f4
Samuel <craftysamuel@gmail.com>
2025-10-01 19:46:04 +0930

    feat: added rebounding of walls with dampening factor and ball asset

--- player.gd
+++ player.gd
@@ -4,6 +4,8 @@ extends Area2D
 
 @export var drag_coefficient = 5 # the deceleration coefficient
 
+@export var dampening = 0.9 # The ratio of speed remaining after colliding with a wall (inelasticity)
+
 var screen_size # Size of the game window.
 
 var velocity = Vector2.ZERO # The player's movement vector.
@@ -20,6 +22,13 @@ func _process(delta):
 		velocity.y += acceleration
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= acceleration
+	
+	# rebound if hit top bottom or sides
+	if position.x == screen_size.x || position.x == 0:
+		velocity.x = velocity.x * -1 * dampening
+	
+	if position.y == screen_size.y || position.y == 0:
+		velocity.y = velocity.y * -1 * dampening
 
 	velocity.x += 0.5 * drag_coefficient / 100000 * velocity.x * abs(velocity.x) * -1 #creates wind resistance in opposite direction to motion 1/2*p*v^2*cd
 	velocity.y += 0.5 * drag_coefficient / 100000 * velocity.y * abs(velocity.y) * -1 #creates wind resistance in opposite direction to motion
